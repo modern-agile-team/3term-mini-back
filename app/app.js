c4384ffdbafb91211c54df2906c5c2d320ad92d7
Yonghoon <yonghoonj98@gmail.com>
2022-01-17 12:06:01 +0900

    Server 폴더 구조화 및 DB 구축 전

--- app/app.js
+++ app/app.js
@@ -1,12 +1,10 @@
+"use strict";
+
 const express = require("express");
 const app = express();
-
-const PORT = 8080;
 
 app.get("/", (req, res) => {
   res.send("hi");
 });
 
-app.listen(PORT, (req, res) => {
-  console.log("서버가동");
-});
+module.exports = app;
