827e5fe70811e7b8dcca76d4f64edc30bba1c960
Yonghoon <yonghoonj98@gmail.com>
2022-01-25 13:31:46 +0900

    Add(yonghoon/report): 신고 화면 카테고리값 전송 API 추가 #27

--- app/src/apis/report/index.js
+++ app/src/apis/report/index.js
@@ -5,6 +5,8 @@ const reportCtrl = require("./report.Ctrl");
 
 const router = express.Router();
 
+router.get("/", reportCtrl.lookUp.lookUpReportPage);
+
 router.post("/board", reportCtrl.process.boardReport);
 router.post("/user", reportCtrl.process.userReport);
 
