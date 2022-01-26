251fa39c19be6f9b655f48d5ee277d6fd1d2f575
subro <suhyung,lee9810@gmail.com>
2022-01-26 17:14:22 +0900

    Add(leesuhyung/login,register)로그인,회원가입 로그관리 추가(혹시 모르니 커밋하지 마세요) #11

--- app/app.js
+++ app/app.js
@@ -2,9 +2,12 @@
 
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+const logger = require("./src/config/logger");
+const accessLogStream = require("./src/config/log");
 
-const app = express();
 dotenv.config();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,10 +18,12 @@ const board = require("./src/apis/board");
 const user = require("./src/apis/user");
 const report = require("./src/apis/report");
 
+app.use(morgan("tiny", { stream: logger.stream }));
 // API 연결
 app.use("/api/profile", profile);
 app.use("/api/board", board);
 app.use("/api/user", user);
 app.use("/api/report", report);
+// app.use(morgan("dev"));
 
 module.exports = app;
--- app/src/models/services/User/User.js
+++ app/src/models/services/User/User.js
@@ -10,17 +10,16 @@ class User {
     try {
       // const { id, password } = await UserStorage.getUserInfo(client.id);
       const userInfo = await UserStorage.getUserInfo(client.id);
-      //
-      if (userInfo.length) {
+      if (userInfo) {
         if (userInfo.password === client.password) {
-          return { success: true };
+          return { success: true, msg: "로그인 성공" };
         }
         return { success: false, msg: "비밀번호가 틀렸습니다" };
       }
+
       return { success: false, msg: "존재하지 않는 아이디입니다." };
     } catch (err) {
-      console.log(err);
-      return { success: false, msg: err };
+      throw { success: false, err };
     }
   }
   async register() {
@@ -28,37 +27,43 @@ class User {
     //id,psword,이름,이 있는지 부터 확인
     const clientObj = {
       id: client.id,
-      psword: client.psword,
+      password: client.password,
       name: client.name,
-      confirm: client.pswordConfirm,
+      confirm: client.passwordConfirm,
     };
     // if(!(client.id && client.psword && client.name&&client.pswordconfirm)) {
-    const nullkeys = Object.keys(clientObj)
+    const nullKeys = Object.keys(clientObj)
       .filter((key) => {
         if (!clientObj[key]) return true;
         return false;
       })
       .join("");
 
-    if (nullkeys)
+    if (nullKeys)
       return {
         success: false,
-        msg: `${nullkeys}에 해당하는 값을 입력해주세요`,
+        msg: `${nullKeys}에 해당하는 값을 입력해주세요`,
       };
-
+    //password === passwordconfirm확인하는 작업
+    if (!(client.password === client.passwordConfirm)) {
+      return {
+        success: false,
+        msg: "비밀번호와 비밀번호 확인이 서로 다릅니다.",
+      };
+    }
     //body에 nickname값이 없다면 id값을 nickname으로 지정해줌
     if (!client.nickname) {
       client.nickname = client.id;
     }
+
     //id 중복 시 다른 id로 입력 할 수 있도록.
     try {
       const id = await UserStorage.getUserInfo(client.id);
-
       return id.length
         ? { success: false, msg: "해당 id는 사용중인 id입니다." }
         : await UserStorage.save(client);
     } catch (err) {
-      return { success: false, msg: err };
+      throw { success: false, err };
     }
   }
 }
