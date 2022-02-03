387ba75224f37b00d7da512591d776a1ed97d8c4
Yonghoon <yonghoonj98@gmail.com>
2022-02-03 11:17:09 +0900

    Modify(yonghoon/profile): 조회 sql문 코드 수정 #1

--- app/src/models/services/Profile/ProfileStorage.js
+++ app/src/models/services/Profile/ProfileStorage.js
@@ -6,11 +6,9 @@ class ProfileStorage {
   static async selectProfile(profileUserNo) {
     try {
       const { userNo } = profileUserNo;
-      const query = `SELECT users.name, users.nickname, users.mail, COUNT(boards.no) AS boards 
-          FROM users 
-          LEFT JOIN boards 
-          ON users.no = boards.user_no 
-          WHERE users.no=?`;
+      const query = `SELECT users.name, users.nickname, users.mail, 
+      (SELECT COUNT(*) FROM boards WHERE users.no = boards.no) AS boards 
+      FROM users WHERE users.no = ?;`;
       const selectResult = await mysql.query(query, [userNo]);
       console.log(selectResult[0]);
       if (selectResult[0].length) {
