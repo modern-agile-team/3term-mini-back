e1570f6a18a9d2c23ddd8f3fc764aaac22b4dcaa
subro <suhyung,lee9810@gmail.com>
2022-02-14 16:17:02 +0900

    Modify(leesuhyung/register): 에러,성공메시지 수정 #11

--- app/src/models/services/User/User.js
+++ app/src/models/services/User/User.js
@@ -61,13 +61,14 @@ class User {
     //id,nickname,mail 중복 시 다른 id로 입력 할 수 있도록.
     try {
       const duplicatedId = await UserStorage.isDuplicatedId(client.id);
+      console.log(duplicatedId);
       const duplicateNickname = await UserStorage.judgeDuplicateNickname(
         client.nickname
       );
       const duplicateMail = await UserStorage.judgeDuplicateMail(client.mail);
 
       const duplicationObj = {
-        id: duplicatedId.success,
+        id: duplicatedId,
         nickname: duplicateNickname.success,
         mail: duplicateMail.success,
       };
